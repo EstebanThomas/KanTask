6f13b3806cf6e14367317c0678dcf4c593094c24
Este <et.thomas@hotmail.fr>
2025-07-09 11:07:19 +0200

    Delete Card

--- kantask/src/app/api/project/[id]/lists/[listId]/cards/[cardId]/route.js
+++ kantask/src/app/api/project/[id]/lists/[listId]/cards/[cardId]/route.js
@@ -1,38 +1,7 @@
 import { connectMongoDB } from "../../../../../../../../../lib/mongo";
 import Card from "../../../../../../../../models/Card";
+import List from "../../../../../../../../models/List"
 
-export async function DELETE(req, { params }) {
-    try {
-        const { id, cardId } = await params;
-
-        await connectMongoDB();
-
-        // delete card
-        const deletedCard = await Card.findByIdAndDelete(cardId);
-
-        if (!deletedCard) {
-            return new Response(JSON.stringify({ error: "Card not found" }), { status: 404 });
-        }
-
-        //ORDER
-        const [rows] = await db.execute("SELECT card_order FROM projects WHERE id = ?", [id]);
-        const currentOrder = rows[0].card_order ? JSON.parse(rows[0].card_order) : [];
-        const newOrder = currentOrder.filter((cId) => cId !== cardId);
-
-        await db.execute("UPDATE projects SET card_order = ? WHERE id = ?", [
-            JSON.stringify(newOrder),
-            id,
-        ]);
-
-        return new Response(
-            JSON.stringify({ message: "Card deleted" }),
-            { status: 200, headers: { "Content-Type": "application/json" } }
-        );
-    } catch (err) {
-        console.error(err);
-        return new Response(JSON.stringify({ error: "Server error" }), { status: 500 });
-    }
-}
 
 export async function PATCH(req, { params }) {
     try {
@@ -54,6 +23,33 @@ export async function PATCH(req, { params }) {
         return new Response(JSON.stringify(updatedCard), {
         status: 200,
         headers: { "Content-Type": "application/json" },
+        });
+    } catch (err) {
+        console.error(err);
+        return new Response(JSON.stringify({ error: "Server error" }), { status: 500 });
+    }
+}
+
+export async function DELETE(req, { params }) {
+    try {
+        const { listId, cardId } = params;
+
+        await connectMongoDB();
+
+        // Delete card
+        const deletedCard = await Card.findByIdAndDelete(cardId);
+        if (!deletedCard) {
+        return new Response(JSON.stringify({ error: "Card not found" }), { status: 404 });
+        }
+
+        // Delete ID in list
+        await List.findByIdAndUpdate(listId, {
+        $pull: { cards: cardId }
+        });
+
+        return new Response(JSON.stringify({ message: "Card deleted" }), {
+        status: 200,
+        headers: { "Content-Type": "application/json" }
         });
     } catch (err) {
         console.error(err);
